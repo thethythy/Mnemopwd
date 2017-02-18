b33d852e3073a398877b1a79baa4ea21b8d62563
thethythy <thethythy@gmail.com>
2017-02-18 10:06:11 +0100

    Bug #17 fixed (client disconnection when too many blocks are received)

--- mnemopwd/server/clients/protocol/StateS34.py
+++ mnemopwd/server/clients/protocol/StateS34.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2016, Thierry Lemeunier <thierry at lemeunier dot net>
+# Copyright (c) 2015-2017, Thierry Lemeunier <thierry at lemeunier dot net>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
@@ -31,6 +31,7 @@ State S34 : search data operation
 
 import pickle
 import logging
+import asyncio
 
 from ...util.funcutils import singleton
 from .StateSCC import StateSCC
@@ -70,6 +71,10 @@ class StateS34(StateSCC):
                     # Send sib
                     msg = b';SIB;' + si + b';' + lpsib + b';' + psib
                     client.loop.call_soon_threadsafe(client.transport.write, msg)
+                    # Wait for sending the message
+                    coro = asyncio.sleep(0.005, loop=client.loop)
+                    future = asyncio.run_coroutine_threadsafe(coro, client.loop)
+                    future.result(1)
 
                 client.state = client.states['3']  # New client state
 
